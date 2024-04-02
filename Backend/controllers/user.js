b145a3d371d21fcd6fa1eaff4fd92946318f9f29
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-02 10:11:46 +0500

    Building Login

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,6 +1,35 @@
 import { User } from "../models/useModel.js";
 import  bcryptjs  from "bcrypt"
 
+export const Login = async(req, res) =>{
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(401).json({
+                message: "Invalid data",
+                success: false
+            })
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                message:"Invalid email or password",
+                success: false
+            })
+        }
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if(!isMatch){
+            return res.status(401).json({
+                message: "Invalid email Or password",
+                success: false
+            })
+        }
+         
+    } catch (error) {
+        
+    }
+}
+
 export const Register = async (req, res) => {
     try {
         const {fullName, email, password} = req.body;
